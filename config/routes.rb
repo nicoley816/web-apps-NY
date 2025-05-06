6e422ea892cfa018c8ca08e68648818a73faecf6
Nicole Yu <nicole.yu@kellogg.northwestern.edu>
2025-05-06 16:30:16 +0000

    worked on companies lab

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # get("/things", :controller => "things", :action => "index")
   get("/tacos", :controller => "tacos", :action => "index")
   get("/dice", :controller => "dice", :action => "index")
+  get("/companies", :controller => "companies", :action => "index")
+  get("/contacts", :controller => "contacts", :action => "index")
 
   # first part is telling you how to find it
   # second/third part is telling you what info
