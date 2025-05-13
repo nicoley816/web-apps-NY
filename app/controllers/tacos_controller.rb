60d9a319a70e7fbba28507ba8faec2b222d958f1
Nicole Yu <nicole.yu@kellogg.northwestern.edu>
2025-05-13 14:05:29 +0000

    worked on this during class

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,5 +1,6 @@
 class TacosController < ApplicationController
   def index
+    @fillings = ["carnitas", "pescado", "al pastor"]
     # render :inline => "<p>Hello!</p>"
     render :template => "tacos/index"
   end
